b2ca5c1aa131e79c0b08039a5abcf600f68f1ed3
remi-or <remi.pierre_o@orange.fr>
2025-01-17 09:53:09 +0000

    Added the skip bench opion

--- bench.py
+++ bench.py
@@ -15,12 +15,14 @@ if __name__ == "__main__":
     parser.add_argument("--arguments", "-a", default="")
     parser.add_argument("--verbose", "-v", action="store_true")
     parser.add_argument("--skip-test", "-st", action="store_true")
+    parser.add_argument("--skip-bench", "-sb", action="store_true")
     args = parser.parse_args()
     project = Project(str(args.project))
     message = str(args.message)
     arguments = str(args.arguments)
     verbose = bool(args.verbose)
     skip_test = bool(args.skip_test)
+    skip_bench = bool(args.skip_bench)
 
     # Compile and run test
     if skip_test:
@@ -29,8 +31,11 @@ if __name__ == "__main__":
         test_output = project.compile_and_run("test.cu", arguments)
     
     # Compile and run benchmark
-    time_string = project.compile_and_run("bench.cu", arguments)
-    times = time_string[2:-3].split(", ")[3:]
+    if skip_bench:
+        times = ["End of warmup", 0]
+    else:
+        time_string = project.compile_and_run("bench.cu", arguments)
+        times = time_string[2:-3].split(", ")[3:]
     
     # Retrieve benchmarked times
     bench_ts, t = [], times.pop()
