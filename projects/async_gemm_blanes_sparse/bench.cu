818f0c9590623069297e9f984648fb9a51a75eaf
remi-or <remi.pierre_o@orange.fr>
2025-01-20 17:02:27 +0000

    Made bench match w/ pytho (regression avoided if STACK=1)

--- projects/async_gemm_blanes_sparse/bench.cu
+++ projects/async_gemm_blanes_sparse/bench.cu
@@ -1,12 +1,15 @@
 #include "./../common.cuh"
 #include "./sparse_k.cu"
 
+#define STACK 10
+#define OUTD half
+
 int main(int argc, char **argv) {
     HIP_CHECK( hipSetDevice(0) );
 
     // Parameters
-    const int iterations = 2500;
-    const int warmups = 200;
+    const int iterations = 3500 / STACK;
+    const int warmups = 500 / STACK;
 
     assert(argc==4);
     const int m = atoi(argv[1]);
@@ -17,7 +20,7 @@ int main(int argc, char **argv) {
     fp8 *hA; fp8* dA, *hB, *dB;
     random_host_tensor<fp8>(hA, m * k);
     random_host_tensor<fp8>(hB, k * n);
-    float *D;
+    OUTD *D;
     // Events 
     hipEvent_t start, stop;
     HIP_CHECK(hipEventCreate(&start));
@@ -32,7 +35,7 @@ int main(int argc, char **argv) {
         // Create tensors
         tensor_h2d<fp8>(hA, dA, m * k);
         tensor_h2d<fp8>(hB, dB, k * n);
-        empty_device_tensor<float>(D, m * n);
+        empty_device_tensor<OUTD>(D, m * n);
         // Flush cache 
         flush_device_cache();
         // Sync
@@ -40,7 +43,10 @@ int main(int argc, char **argv) {
 
         // Call and time kernel
         HIP_CHECK(hipEventRecord(start));
-        async_gemm(dA, dB, D, m, n, k);
+        #pragma unroll
+        for (int i = 0; i < STACK; i++) {
+            async_gemm(dA, dB, D, m, n, k);
+        }
         HIP_CHECK(hipEventRecord(stop));
         HIP_CHECK(hipEventSynchronize(stop));
         HIP_CHECK(hipEventElapsedTime(&t, start, stop));
@@ -48,7 +54,7 @@ int main(int argc, char **argv) {
         if (iter == warmups) {
             std::cout << "End of warmup, ";
         }
-        std::cout << t * 1000 << ", ";
+        std::cout << t * 1000 / STACK << ", ";
 
         // Free tensors
         HIP_CHECK(hipFree(dA));
