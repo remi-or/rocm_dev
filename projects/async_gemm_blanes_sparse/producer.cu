d858fb992a7937f038e026b01a22985c947e9fa3
remi-or <remi.pierre_o@orange.fr>
2025-02-03 11:49:02 +0000

    Added A prod unrolling

--- projects/async_gemm_blanes_sparse/producer.cu
+++ projects/async_gemm_blanes_sparse/producer.cu
@@ -31,93 +31,84 @@ void __device__ _tsr_A_producer(
     buffer += (E_P_BANK * curr_ad) + (curr_ld % 64) * 2 + (curr_ld / 64) * (32*E_P_BANK * 4);
     
     // Prepare registers
-    fp8 reg[elems_per_thread];
+    fp8_4 reg0[elems_per_thread / 4];
+    fp8_4 reg1[elems_per_thread / 4];
+    fp8_4 swap_reg[elems_per_thread / 4];
 
     // K-wise loop
-    fp8* buf;
+    fp8_4* buf;
     int b = (OPS == 1 ? 2 : 1) * role_id;
 
     while (b < k_blocks) {
         // Account for cyclic queue
         index -= (index >= QSIZE) ? QSIZE : 0;
-        buf = buffer + index * (WARPTILE_M * WARPTILE_K);
+        buf = reinterpret_cast<fp8_4*>(buffer) + index * (WARPTILE_M * WARPTILE_K / 4);
 
-        #pragma unroll
-        for (int op = 0; op < OPS; op+=2) {
+        // Load from gmem to reg
+        asm volatile(
+            "global_load_dwordx4 %0, %2, off offset:0  \n\t"
+            "global_load_dwordx4 %1, %2, off offset:128\n\t"
+            : "=v"(reg0), "=v"(reg1)
+            : "v"(src)
+        );
 
-            // Load from gmem to reg
-            #pragma unroll
-            for (int i = 0; i < elems_per_thread; i++) {
-                reg[i] = src[i];
-            }
-            // Advance
-            src += 2 * OP_K;
-
-            if (op == 0) {
-                // Wait for buffer to be consumed
-                while (queue[2 * B_LANES * index] != p_state) {
-                    asm volatile("s_sleep 0");
-                }
-            }
-
-            // Store in smem from reg
-            if ((OPS % 2 == 0) || (op != OPS - 1) || (threadIdx.x % 8 < 4)) {
-                #pragma unroll
-                for (int line = 0; line < 4; line++) {
-                    #pragma unroll
-                    for (int j = 0; j < 4; j++){
-                        buf[line * 32*E_P_BANK + j] = reg[8*(line%2) + 2*(line/2) + j + 2*(j/2)];
-                    }
-                }
-            }
-            buf += (OPS == 1 ? 0 : 2 * OP_K * OP_M);
+        // Wait for buffer to be consumed
+        while (queue[2 * B_LANES * index] != p_state) {
+            asm volatile("s_sleep 0");
         }
+
+        // Store in smem from reg
+        asm volatile(
+            "s_waitcnt vmcnt(1)\n\t"
+            "v_pack_b32_f16 %0, %4, %5, op_sel:[0,0]\n\t"
+            "v_pack_b32_f16 %1, %4, %5, op_sel:[1,1]\n\t"
+            "v_pack_b32_f16 %2, %6, %7, op_sel:[0,0]\n\t"
+            "v_pack_b32_f16 %3, %6, %7, op_sel:[1,1]\n\t"
+            "v_swap_b32 %0, %4\n\t"
+            "v_swap_b32 %1, %6\n\t"
+            "v_swap_b32 %2, %5\n\t"
+            "v_swap_b32 %3, %7"
+            : "=v"(swap_reg[0]), "=v"(swap_reg[1]), "=v"(swap_reg[2]), "=v"(swap_reg[3])
+            : "v"(reg0[0]), "v"(reg0[1]), "v"(reg0[2]), "v"(reg0[3])
+        );
+        #pragma unroll
+        for (int line = 0; line < 4; line++) {
+            buf[line * 32] = reg0[line];
+        }
+
+        // Store in smem from reg
+        asm volatile(
+            "s_waitcnt vmcnt(0)\n\t"
+            "v_pack_b32_f16 %0, %4, %5, op_sel:[0,0]\n\t"
+            "v_pack_b32_f16 %1, %4, %5, op_sel:[1,1]\n\t"
+            "v_pack_b32_f16 %2, %6, %7, op_sel:[0,0]\n\t"
+            "v_pack_b32_f16 %3, %6, %7, op_sel:[1,1]\n\t"
+            "v_swap_b32 %0, %4\n\t"
+            "v_swap_b32 %1, %6\n\t"
+            "v_swap_b32 %2, %5\n\t"
+            "v_swap_b32 %3, %7"
+            : "=v"(swap_reg[0]), "=v"(swap_reg[1]), "=v"(swap_reg[2]), "=v"(swap_reg[3])
+            : "v"(reg1[0]), "v"(reg1[1]), "v"(reg1[2]), "v"(reg1[3])
+        );
+        #pragma unroll
+        for (int line = 0; line < 4; line++) {
+            buf[line * 32 + OP_K * OP_M / 2] = reg1[line];
+        }
+
         // Mark buffer as filled
         queue[2 * B_LANES * index] = p_state + 1;
-        buf += (OPS == 1 ? 0 : 2 * OP_K * OP_M);
 
-#if OPS > 1
         // Advance
-        src += WARPTILE_K * (A_PRODUCERS - 1) - (OPS % 2 == 1 ? OP_K : 0);
+        src += WARPTILE_K * A_PRODUCERS;
 
         // Update index
         index += A_PRODUCERS;
         p_state = (index >= QSIZE) ? (p_state+2) : p_state;
         b += A_PRODUCERS;
-#else
-        // Second buffer //
-
-        // Update index
-        index += 1;
-        // Wait for buffer to be consumed
-        while (queue[2 * B_LANES * index] != p_state) {
-            asm volatile("s_sleep 0");
-        }
-        // Store in smem from reg
-        if (threadIdx.x % 8 > 3) {
-            #pragma unroll
-            for (int line = 0; line < 4; line++) {
-                #pragma unroll
-                for (int j = 0; j < 4; j++){
-                    buf[line * 32*E_P_BANK + j] = reg[8*(line%2) + 2*(line/2) + j + 2*(j/2)];
-                }
-            }
-        }
-        // Mark buffer as filled
-        queue[2 * B_LANES * index] = p_state + 1;
-
-        // Advance
-        src += 2 * OP_K * (A_PRODUCERS - 1);
-
-        // Update index
-        index += 2*A_PRODUCERS - 1;
-        p_state += (index >= QSIZE) ? p_state + 2 : 0;
-        b += 2*A_PRODUCERS;
-#endif
     }
 
     // Bring warps back in order
-    role_id = (b - k_blocks) / (OPS == 1 ? 2 : 1); // WARNING: not sure about this (and it's late)
+    role_id = (b - k_blocks); // WARNING: not sure about this (and it's late)
 }
 
 template<int B_PRODUCERS, int B_LANES, int QSIZE>
